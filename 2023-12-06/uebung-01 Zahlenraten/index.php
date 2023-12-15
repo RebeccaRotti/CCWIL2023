df2c1003a7169f0e9108dfc19466a6bbe0528998
Rebecca Rottensteiner <37736836+RebiRotti@users.noreply.github.com>
2023-12-15 09:24:46 +0100

    Zahlenraten in PHP

--- 2023-12-06/uebung-01 Zahlenraten/index.php
+++ 2023-12-06/uebung-01 Zahlenraten/index.php
@@ -1,47 +1,63 @@
 <?php
 // Start der Sitzung, um Daten über Seitenaufrufe hinweg zu speichern
-// TODO
+session_start();
 
 // Wenn der "Zurücksetzen"-Button geklickt wurde
-// TODO
+if(isset($_POST['reset'])) {
     // Sitzung zerstören und zur Hauptseite zurückkehren
-    // TODO
-    // TODO // Um die Seite neu zu laden
-    // TODO
+    session_destroy();
+    // Um die Seite neu zu laden
+    header("Location: index.php");
+    exit;
+}
 
 
 // Überprüfung, ob die Zielnummer bereits in der Sitzung existiert
-// TODO
+if(!isset($_SESSION['targetNumber'])) {
     // Wenn keine Zielnummer vorhanden ist, generieren Sie eine neue
-    // TODO
-    // TODO // Initialisieren des Arrays für Versuche
-
+    $_SESSION['targetNumber'] = generateRandomNumber();
+    // Initialisieren des Arrays für Versuche
+    $_SESSION['attempts'] = array();
+}
 
 // Die generierenRandomNumber-Funktion erzeugt eine Zufallszahl zwischen 1 und 100
 function generateRandomNumber() {
-    // TODO
+    return rand(1, 100);
 }
 
 // Die playGame-Funktion vergleicht den geratenen Wert mit der Zielnummer und gibt eine Nachricht zurück
 function playGame($targetNumber, $guess) {
-    // TODO
+    if($guess > $targetNumber) {
+        return "Zu groß";
+    } elseif($guess < $targetNumber) {
+        return "Zu klein";
+    } else {
+        return "Richtig!";
+    }
 }
 
 // Funktion zum Verwalten des gesamten Spiels
 function runGame() {
-// TODO
-// Laden der vorherigen Versuche
-// TODO
+    // Auslesen der targetNumber aus der Session
+    $targetNumber = $_SESSION['targetNumber'];
+    // Laden der vorherigen Versuche
+    $attempts = $_SESSION['attempts'];
 
+    if(isset($_POST['guess'])) {
 
-    // Den aktuellen Versuch zum Array der vorherigen Versuche hinzufügen
-    // TODO
+        $guess = intval($_POST['guess']);
+        $result = playGame($targetNumber, $guess);
 
-        // Wenn das Spiel gewonnen wurde, Zielnummer zurücksetzen
-    // TODO
-
+        // Den aktuellen Versuch zum Array der vorherigen Versuche hinzufügen
+        $attempts[] = array('guess' => $guess, 'result' => $result);
+        if($result === 'Richtig!') {
+            // Wenn das Spiel gewonnen wurde, Zielnummer zurücksetzen
+            unset($_SESSION['targetNumber']);
+        }
+    }
     // Aktualisieren der vorherigen Versuche
-    // TODO
+    $_SESSION['attempts'] = $attempts;
+
 
 ?>
 
@@ -67,9 +83,24 @@ function runGame() {
         </div>
         <button type="submit" class="btn btn-primary">Schätzen</button>
     </form>
-    <?php } ?>
+
+    <?php
+        if(isset($result)) {
+            echo '<div class="alert alert-info mt-3">' . $result . '</div>';
+        }
+        if(!empty($attempts)) {
+            echo '<div class="mt-3"><h2>Vorherige Versuche.</h2>';
+            echo '<ul>';
+            foreach($attempts as $attempt) {
+                // var_dump($attempt);
+                echo '<li>Die Schätzung: ' . $attempt['guess'] . ', Ergebnis: ' . $attempt['result'] . '</li>';
+            }
+            echo '</ul></div>';
+        }
+    ?>
+
     </body>
     </html>
-    <?php
-    // TODO
-?>
+
+    <?php } ?>
+    <?php runGame(); ?>
